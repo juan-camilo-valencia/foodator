83a67020f39be2eb4eb9810fc60fe09def812bea
Brandon Ramos <brandon.ramos@cadena.com.co>
2022-10-28 17:23:57 -0500

    v400

---
+++ app/src/main/java/com/example/foodatorapp/Activity/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.foodatorapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.foodatorapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+@SuppressLint("WrongViewCast")
+private ConstraintLayout startBtn = findViewById(R.id.startBtn);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
